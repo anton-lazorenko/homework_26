8c1c8891b056e4317308ce0797879dd54287a4ed
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-03-04 06:46:32 +0200

    finished 2 task

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict"
 // Task 1
-function getCount(str) {
+function getCount(letters) {
     let countVowels = 0;
     const vowels = ['a', 'e', 'i', 'o', 'u']
 
-    for (let char of str) {
-        if (vowels.includes(char)) {
+    for (let amount of letters) {
+        if (vowels.includes(amount)) {
             countVowels++
         }
     }
@@ -13,4 +13,22 @@ function getCount(str) {
 }
 
 getCount("Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dicta perspiciatis aperiam eum tempora fugiat itaque obcaecati temporibus, voluptatibus quo.")
- 
+
+
+// Task 2
+function moveOver(array, direction) {
+    let arr = array
+    if (direction === 'start'){
+       let elem = arr.shift() // удаляем первый эллемент массива
+       arr.push(elem) // добавляем эллемент в конец массива
+       return alert(arr)
+
+    } else if (direction === 'end'){
+        let elem = arr.pop()
+        arr.unshift(elem)
+        return alert(arr)
+    }  
+} 
+
+moveOver(['China', 'USA', 'Norway', 'Bulgaria'], 'start') // переместить в начало
+// moveOver(['China', 'USA', 'Norway', 'Bulgaria'], 'end') // переместить в конец
