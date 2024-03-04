f9268cab0ec6c1154ad671650f3f81c7e404192f
Anton Lazorenko <lazorenkoanton95@gmail.com>
2024-03-04 11:04:57 +0200

    finished task 3

--- script.js
+++ script.js
@@ -18,17 +18,41 @@ getCount("Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum dicta per
 // Task 2
 function moveOver(array, direction) {
     let arr = array
-    if (direction === 'start'){
-       let elem = arr.shift() // удаляем первый эллемент массива
-       arr.push(elem) // добавляем эллемент в конец массива
-       return alert(arr)
+    if (direction === 'start') {
+        let elem = arr.shift() // удаляем первый эллемент массива
+        arr.push(elem) // добавляем эллемент в конец массива
+        return alert(arr)
 
-    } else if (direction === 'end'){
+    } else if (direction === 'end') {
         let elem = arr.pop()
         arr.unshift(elem)
         return alert(arr)
-    }  
-} 
+    }
+}
 
 moveOver(['China', 'USA', 'Norway', 'Bulgaria'], 'start') // переместить в начало
 // moveOver(['China', 'USA', 'Norway', 'Bulgaria'], 'end') // переместить в конец
+
+// TASK 3 
+
+const findLongestWord = function(str) {
+    let arrStr = str.split(' ');
+    let wordLength = 0;
+    let longestWord;
+
+    for (let i = 0; i < arrStr.length; i += 1) {
+        wordLength = arrStr[1].length;
+
+        if (arrStr[i].length > wordLength) {
+            longestWord = arrStr[i];
+
+            return longestWord;
+        }
+    }
+};
+
+console.log(findLongestWord("Необязательный параметр. Указывает символы, используемые в качестве разделителя внутри строки. Параметр separator может быть как строкой, так и регулярным выражением. Если параметр опущен, возвращённый массив будет содержать один элемент со всей строкой. Если параметр равен пустой строке, строка str будет преобразована в массив символов."));
+console.log(findLongestWord("Оператор (операторная функция) new создаёт экземпляр объекта, встроенного или определённого пользователем, имеющего конструктор."));
+console.log(findLongestWord("Как найти первую работу в IT?"));
+ 
+
